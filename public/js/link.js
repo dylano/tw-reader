b59c82ca4d062224ab4557b9a8b2a25cddd113de
Dylan Oliver <d@doliver.net>
2018-03-28 21:08:32 -0700

    grid

--- public/js/link.js
+++ public/js/link.js
@@ -1,4 +1,4 @@
-const linker = new Autolinker();
+const linker = new Autolinker({ mention: "twitter" });
 const allTweets = document.querySelectorAll(".tweetstr");
 allTweets.forEach(el => {
   el.innerHTML = linker.link(el.innerHTML);
