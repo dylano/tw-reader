0af2526bee43e6a5ae55cd84a9a78155e790dd4e
Dylan Oliver <d@doliver.net>
2018-10-30 17:53:08 -0700

    remove swipe (too touchy), add action icons

--- react-app/src/TweetPanel.js
+++ react-app/src/TweetPanel.js
@@ -1,11 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Swipe from "react-easy-swipe";
 import "./TweetPanel.css";
 
 const TweetPanel = ({ friend, tweets, onTweetRead, showAllTweets = false }) => {
   if (!showAllTweets) {
-    tweets = tweets.filter(tweet => tweet.isRead === false);
+    tweets = tweets.filter(tweet => !tweet.isRead);
   }
 
   let content;
@@ -13,13 +12,25 @@ const TweetPanel = ({ friend, tweets, onTweetRead, showAllTweets = false }) => {
     content = <div className="tweet-panel-message">No tweetz :(</div>;
   } else {
     content = tweets.sort((a, b) => a.timestamp.localeCompare(b.timestamp)).map(tweet => {
-      const className = tweet.isRead ? "tweet-panel-tweet" : "tweet-panel-tweet tweet-panel-tweet-new";
+      const className = tweet.isRead
+        ? "tweet-panel-tweet"
+        : "tweet-panel-tweet tweet-panel-tweet-new";
+      const tweetLink = `https://twitter.com/${friend.screenName}/status/${tweet.id}`;
       return (
-        <Swipe onSwipeRight={() => onTweetRead(tweet._id)} key={tweet._id}>
-          <div className={className} key={tweet._id}>
-            {tweet.text}
-          </div>
-        </Swipe>
+        <div className={className} key={tweet._id}>
+          <span className="tweet-action action-open action-left">
+            <a href={tweetLink} target="_blank" rel="noopener noreferrer">
+              <i class="fas fa-dove" />
+            </a>
+          </span>
+          <div className="tweet-panel-tweet-content">{tweet.text}</div>
+          <span
+            className="tweet-action action-close action-right"
+            onClick={() => onTweetRead(tweet._id)}
+          >
+            <i class="far fa-times-circle" />
+          </span>
+        </div>
       );
     });
   }
