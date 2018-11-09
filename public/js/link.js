8bdaec0e65dc3b4c9dd6d739312ed572d327c8e2
Dylan Oliver <d@doliver.net>
2018-11-08 18:01:53 -0800

    Fixes #12. keyboard shortcut to open link w/in tweet.

--- public/js/link.js
+++ public/js/link.js
@@ -1,6 +1,6 @@
 /* global Autolinker */
-const linker = new Autolinker({ mention: "twitter" });
-const allTweets = document.querySelectorAll(".tweetstr");
+const linker = new Autolinker({ mention: 'twitter', className: 'autolink' });
+const allTweets = document.querySelectorAll('.tweetstr');
 allTweets.forEach(el => {
   el.innerHTML = linker.link(el.innerHTML);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,19 @@
-const tweetItems = [...document.querySelectorAll("div li")];
+const tweetItems = [...document.querySelectorAll('div li')];
 let selectedIndex = -1;
 
 function markItemRead(li) {
   // remove list item from page and mark tweet as read in DB
-  const tweetId = li.getAttribute("data-key");
+  const tweetId = li.getAttribute('data-key');
   const ul = li.parentElement;
   const index = tweetItems.indexOf(li);
   tweetItems.splice(index, 1);
   li.remove();
-  fetch(`/tweets/${tweetId}`, { method: "put" });
+  fetch(`/tweets/${tweetId}`, { method: 'put' });
 
   // if there are no visible items left, hide the section
   let hasChildren = false;
   ul.childNodes.forEach(cnode => {
-    if (!hasChildren && cnode.nodeName === "LI") {
+    if (!hasChildren && cnode.nodeName === 'LI') {
       hasChildren = true;
     }
   });
@@ -23,12 +23,12 @@ function markItemRead(li) {
 }
 
 function markUserRead(div) {
-  const screenName = div.getAttribute("data-key");
-  div.classList.add("hidden");
-  fetch(`/friends/${screenName}`, { method: "PUT" });
+  const screenName = div.getAttribute('data-key');
+  div.classList.add('hidden');
+  fetch(`/friends/${screenName}`, { method: 'PUT' });
 
   // remove the elements from the tweetItems array and adjust the selection
-  const children = div.querySelectorAll("li");
+  const children = div.querySelectorAll('li');
   children.forEach(child => {
     const index = tweetItems.indexOf(child);
     tweetItems.splice(index, 1);
@@ -43,7 +43,7 @@ function markUserRead(div) {
   } else if (selectedIndex > tweetItems.length) {
     selectedIndex = tweetItems.length - 1;
   }
-  tweetItems[selectedIndex].classList.add("selected");
+  tweetItems[selectedIndex].classList.add('selected');
 }
 
 function markReadItemClicked() {
@@ -55,57 +55,62 @@ function markReadUserClicked() {
 }
 
 function showLoader() {
-  const loader = document.querySelector("#load-animation");
-  loader.classList.remove("not-loading");
-  loader.classList.add("loading");
+  const loader = document.querySelector('#load-animation');
+  loader.classList.remove('not-loading');
+  loader.classList.add('loading');
 }
 
 function processKeypress(event) {
   const keypress = event.key;
-  if (keypress === "?") {
+  if (keypress === '?') {
     alert(
-      "G - retrieve new tweets\nj / k - down/up\no - open current on Twitter\nx - mark tweet read\nX - mark all read for current user"
+      'G - retrieve new tweets\nj / k - down/up\no - open current on Twitter\nL - open URL embedded in tweet\nx - mark tweet read\nX - mark all read for current user'
     );
-  } else if (["j", "k", "o", "x", "X", "G", "-", "="].includes(keypress)) {
+  } else if (['j', 'k', 'l', 'L', 'o', 'x', 'X', 'G', '-', '='].includes(keypress)) {
     // page actions
-    if (keypress === "G") {
+    if (keypress === 'G') {
       showLoader();
-      document.querySelector("#btnNewTweets").submit();
+      document.querySelector('#btnNewTweets').submit();
     }
 
     // nav / item actions
     if (selectedIndex === -1) {
       selectedIndex = 0;
-      tweetItems[selectedIndex].classList.add("selected");
-    } else if (keypress === "j" && selectedIndex < tweetItems.length - 1) {
-      tweetItems[selectedIndex].classList.remove("selected");
-      tweetItems[++selectedIndex].classList.add("selected");
-    } else if (keypress === "k" && selectedIndex > 0) {
-      tweetItems[selectedIndex].classList.remove("selected");
-      tweetItems[--selectedIndex].classList.add("selected");
-    } else if (keypress === "o") {
-      tweetItems[selectedIndex].querySelector(".twitterlink").click();
-    } else if (keypress === "x") {
+      tweetItems[selectedIndex].classList.add('selected');
+    } else if (keypress === 'j' && selectedIndex < tweetItems.length - 1) {
+      tweetItems[selectedIndex].classList.remove('selected');
+      tweetItems[++selectedIndex].classList.add('selected');
+    } else if (keypress === 'k' && selectedIndex > 0) {
+      tweetItems[selectedIndex].classList.remove('selected');
+      tweetItems[--selectedIndex].classList.add('selected');
+    } else if (keypress === 'o') {
+      tweetItems[selectedIndex].querySelector('.twitterlink').click();
+    } else if (keypress === 'x') {
       markItemRead(tweetItems[selectedIndex]);
       if (selectedIndex === tweetItems.length) {
-        tweetItems[--selectedIndex].classList.add("selected");
+        tweetItems[--selectedIndex].classList.add('selected');
       } else {
-        tweetItems[selectedIndex].classList.add("selected");
+        tweetItems[selectedIndex].classList.add('selected');
       }
-    } else if (keypress === "X") {
+    } else if (keypress === 'X') {
       markUserRead(tweetItems[selectedIndex].parentElement.parentElement);
+    } else if (keypress === 'l' || keypress === 'L') {
+      const link = tweetItems[selectedIndex].querySelector('.autolink-url');
+      if (link) {
+        link.click();
+      }
     }
 
     tweetItems[selectedIndex].scrollIntoView({
-      behavior: "smooth",
-      block: "center"
+      behavior: 'smooth',
+      block: 'center'
     });
   }
 }
 
 function highlightSelection() {
   // remove any existing highlight
-  tweetItems.forEach(item => item.classList.remove("selected"));
+  tweetItems.forEach(item => item.classList.remove('selected'));
 
   // create new highlight -- this is more complex than it should be because some
   // clicks are from the button to remove a tweet. So we need to check if the
@@ -114,29 +119,26 @@ function highlightSelection() {
   selectedIndex = tweetItems.indexOf(this);
   if (selectedIndex >= 0) {
     // the simple case
-    tweetItems[selectedIndex].classList.add("selected");
+    tweetItems[selectedIndex].classList.add('selected');
   } else if (selectedIndex === -1 && prevSelection !== -1) {
     // the delete case
-    selectedIndex =
-      prevSelection >= tweetItems.length
-        ? tweetItems.length - 1
-        : prevSelection;
-    tweetItems[selectedIndex].classList.add("selected");
+    selectedIndex = prevSelection >= tweetItems.length ? tweetItems.length - 1 : prevSelection;
+    tweetItems[selectedIndex].classList.add('selected');
   }
 }
 
-document.querySelectorAll(".markreaditem").forEach(btn => {
-  btn.addEventListener("click", markReadItemClicked);
+document.querySelectorAll('.markreaditem').forEach(btn => {
+  btn.addEventListener('click', markReadItemClicked);
 });
 
-document.querySelectorAll(".markreaduser").forEach(btn => {
-  btn.addEventListener("click", markReadUserClicked);
+document.querySelectorAll('.markreaduser').forEach(btn => {
+  btn.addEventListener('click', markReadUserClicked);
 });
 
 tweetItems.forEach(item => {
-  item.addEventListener("click", highlightSelection);
+  item.addEventListener('click', highlightSelection);
 });
 
-document.querySelector("#btnNewTweets").addEventListener("click", showLoader);
+document.querySelector('#btnNewTweets').addEventListener('click', showLoader);
 
-window.addEventListener("keydown", processKeypress);
+window.addEventListener('keydown', processKeypress);
