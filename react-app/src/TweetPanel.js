22ba73959daa94352835d32798b88cfa5635c509
Dylan Oliver <d@doliver.net>
2019-10-20 12:18:44 -0700

    font/padding changes

--- react-app/src/TweetPanel.js
+++ react-app/src/TweetPanel.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ReactAutolinker from 'react-autolinker';
-import './TweetPanel.css';
+import React from "react";
+import PropTypes from "prop-types";
+import ReactAutolinker from "react-autolinker";
+import "./TweetPanel.css";
 
-const autolinkerOptions = { className: 'autolinker' };
+const autolinkerOptions = {className: "autolinker"};
 
-const TweetPanel = ({ friend, tweets, onTweetRead, onUserRead, showAllTweets = false }) => {
+const TweetPanel = ({friend, tweets, onTweetRead, onUserRead, showAllTweets = false}) => {
   if (!showAllTweets) {
     tweets = tweets.filter(tweet => !tweet.isRead);
   }
@@ -14,48 +14,50 @@ const TweetPanel = ({ friend, tweets, onTweetRead, onUserRead, showAllTweets = f
   if (tweets.length === 0) {
     content = <div className="tweet-panel-tweet tweet-panel-tweet-content">No tweetz :(</div>;
   } else {
-    content = tweets.sort((a, b) => a.timestamp.localeCompare(b.timestamp)).map(tweet => {
-      const tweetClass = tweet.isRead
-        ? 'tweet-panel-tweet-content'
-        : 'tweet-panel-tweet-content tweet-panel-tweet-new';
-      const tweetLink = `https://twitter.com/${friend.screenName}/status/${tweet.id}`;
-      const markReadAction = tweet.isRead ? (
-        <span />
-      ) : (
-        <span
-          className="tweet-action action-close action-right"
-          onClick={() => onTweetRead(tweet._id)}
-        >
-          <i className="far fa-check-circle" />
-        </span>
-      );
+    content = tweets
+      .sort((a, b) => a.timestamp.localeCompare(b.timestamp))
+      .map(tweet => {
+        const tweetClass = tweet.isRead
+          ? "tweet-panel-tweet-content"
+          : "tweet-panel-tweet-content tweet-panel-tweet-new";
+        const tweetLink = `https://twitter.com/${friend.screenName}/status/${tweet.id}`;
+        const markReadAction = tweet.isRead ? (
+          <span />
+        ) : (
+          <span
+            className="tweet-action action-close action-right"
+            onClick={() => onTweetRead(tweet._id)}
+          >
+            <i className="far fa-check-circle" />
+          </span>
+        );
 
-      // similarity display if this looks like a duplicate tweet
-      // todo: add a property for similarity display threshold once we've done a test drive to decide on best value
-      let similarityReport = '';
-      if (tweet.similarity > 0.5) {
-        similarityReport = (
-          <div className="tweet-panel-tweet-similarity">
-            ({tweet.similarity}) {tweet.similarityString}
+        // similarity display if this looks like a duplicate tweet
+        // todo: add a property for similarity display threshold once we've done a test drive to decide on best value
+        let similarityReport = "";
+        if (tweet.similarity > 0.5) {
+          similarityReport = (
+            <div className="tweet-panel-tweet-similarity">
+              ({tweet.similarity}) {tweet.similarityString}
+            </div>
+          );
+        }
+
+        return (
+          <div className="tweet-panel-tweet" key={tweet._id}>
+            <span className="tweet-action action-open action-left">
+              <a href={tweetLink} target="_blank" rel="noopener noreferrer">
+                <i className="fas fa-dove" />
+              </a>
+            </span>
+            <div className={tweetClass}>
+              <ReactAutolinker text={tweet.text} options={autolinkerOptions} />
+              {similarityReport}
+            </div>
+            {markReadAction}
           </div>
         );
-      }
-
-      return (
-        <div className="tweet-panel-tweet" key={tweet._id}>
-          <span className="tweet-action action-open action-left">
-            <a href={tweetLink} target="_blank" rel="noopener noreferrer">
-              <i className="fas fa-dove" />
-            </a>
-          </span>
-          <div className={tweetClass}>
-            <ReactAutolinker text={tweet.text} options={autolinkerOptions} />
-            {similarityReport}
-          </div>
-          {markReadAction}
-        </div>
-      );
-    });
+      });
   }
   return (
     <div className="tweet-panel">
