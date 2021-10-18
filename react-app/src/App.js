4220bd1684334beb5c398a195a235a470a61e3f6
Dylan Oliver <d@doliver.net>
2021-10-18 07:52:29 -0700

    function component conversion (#89)
    * function component conversion
    
    * explicit loading states

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import TweetPanel from './components/TweetPanel';
@@ -9,145 +9,141 @@ import './App.css';
 const USE_FAKE_DATA = 0;
 const URL_BASE = process.env.REACT_APP_URL_BASE ?? ''; // if not defined, will proxy calls to localhost:5000
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selectedFriend: null,
-      isFetchingData: false,
-      showAllTweets: false,
-      error: ``,
-      friends: [],
-    };
-  }
+const App = () => {
+  const [selectedFriend, setSelectedFriend] = useState(null);
+  const [isFetchingData, setIsFetchingData] = useState(false);
+  const [showAllTweets, setShowAllTweets] = useState(false);
+  const [error, setError] = useState(``);
+  const [friends, setFriends] = useState([]);
 
-  onError(err) {
+
+  const onError = (err) => {
     console.log(`ERROR: ${err}`);
-    this.setState({ error: `Error: ${err}` });
+    setError(`Error: ${err}`);
   }
 
-  componentDidMount() {
-    if (USE_FAKE_DATA) {
-      this.setState({ ...staticData, error: 'static-data' });
-    } else {
-      this.getTweetData();
-    }
-  }
-
-  getTweetData = () => {
+  const getTweetData = () => {
     fetch(`${URL_BASE}/api/tweets`)
       .then((res) => res.json())
       .then((json) => {
-        this.setState({ ...json });
+        setFriends(json.friends);
       })
-      .catch((err) => this.onError(err));
+      .catch((err) => onError(err));
   };
 
-  getFriend(friendId) {
+  useEffect(() => {
+    if (USE_FAKE_DATA) {
+      setFriends(staticData.friends);
+      setError(`static-data`);
+    } else {
+      getTweetData();
+    }
+  }, []);
+
+  const getFriend = (friendId) => {
     if (!friendId) {
       return null;
     }
-    const friendObj = this.state.friends.find((friend) => friend.friend._id === friendId);
+    const friendObj = friends.find((friend) => friend.friend._id === friendId);
     return friendObj ? friendObj.friend : null;
   }
 
-  getTweetsByFriendId(friendId) {
+  const getTweetsByFriendId = (friendId) => {
     if (!friendId) {
       return null;
     }
-    const friendObj = this.state.friends.find((friend) => friend.friend._id === friendId);
+    const friendObj = friends.find((friend) => friend.friend._id === friendId);
     return friendObj ? friendObj.tweets : [];
   }
 
-  onFriendSelect = (friendId) => {
-    this.setState({ selectedFriend: friendId });
+  const onFriendSelect = (friendId) => {
+    setSelectedFriend(friendId);
   };
 
-  onChangeShowAllTweets = () => {
-    this.setState({ showAllTweets: !this.state.showAllTweets });
+  const onChangeShowAllTweets = () => {
+    setShowAllTweets(!showAllTweets);
   };
 
-  onRefreshTweets = async () => {
-    this.setState({ isFetchingData: !this.state.isFetchingData });
+  const onRefreshTweets = async () => {
+    setIsFetchingData(true);
     await fetch(`${URL_BASE}/api/tweets`, { method: 'POST' });
-    await this.getTweetData();
-    this.setState({ isFetchingData: !this.state.isFetchingData });
+    await getTweetData();
+    setIsFetchingData(false);
   };
 
-  modifyTweetState = (tweetId, change) => {
-      // find the tweet in state and apply the change
-      const newFriends = this.state.friends.map((friend) => {
-        const tweetIndex = friend.tweets.findIndex((tweet) => tweet._id === tweetId);
-        if (tweetIndex >= 0) {
-          const updatedTweet = {};
-          Object.assign(updatedTweet, friend.tweets[tweetIndex], change);
-          friend.tweets = [
-            ...friend.tweets.slice(0, tweetIndex),
-            updatedTweet,
-            ...friend.tweets.slice(tweetIndex + 1, friend.tweets.length),
-          ];
-        }
-        return friend;
-      });
-      this.setState({ friends: newFriends });
-  }
-
-  sendTweetChangeAction = async (tweetId, action) => fetch(`${URL_BASE}/api/tweets/${tweetId}`, { 
-      method: 'PUT',  // todo: this should be PATCH
-      body: JSON.stringify({ action }),
-      headers: { 'Content-Type': 'application/json' },
+  const modifyTweetState = (tweetId, change) => {
+    // find the tweet in state and apply the change
+    const newFriends = friends.map((friend) => {
+      const tweetIndex = friend.tweets.findIndex((tweet) => tweet._id === tweetId);
+      if (tweetIndex >= 0) {
+        const updatedTweet = {};
+        Object.assign(updatedTweet, friend.tweets[tweetIndex], change);
+        friend.tweets = [
+          ...friend.tweets.slice(0, tweetIndex),
+          updatedTweet,
+          ...friend.tweets.slice(tweetIndex + 1, friend.tweets.length),
+        ];
+      }
+      return friend;
     });
-
-  onTweetRead = async (tweetId) => {
-    this.modifyTweetState(tweetId, { isRead: true });
-    await this.sendTweetChangeAction(tweetId, 'read');
-  };
-
-  onTweetSave = async (tweetId, newState) => {
-    this.modifyTweetState(tweetId, { isSaved: newState });
-    await this.sendTweetChangeAction(tweetId, 'save');
+    setFriends(newFriends);
   }
 
-  onUserRead = async (screenName) => {
-    this.setState({ isFetchingData: !this.state.isFetchingData });
+  const sendTweetChangeAction = async (tweetId, action) => fetch(`${URL_BASE}/api/tweets/${tweetId}`, {
+    method: 'PUT',  // todo: this should be PATCH
+    body: JSON.stringify({ action }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  const onTweetRead = async (tweetId) => {
+    modifyTweetState(tweetId, { isRead: true });
+    await sendTweetChangeAction(tweetId, 'read');
+  };
+
+  const onTweetSave = async (tweetId, newState) => {
+    modifyTweetState(tweetId, { isSaved: newState });
+    await sendTweetChangeAction(tweetId, 'save');
+  }
+
+  const onUserRead = async (screenName) => {
+    setIsFetchingData(true);
     const body = JSON.stringify({ action: 'markRead' });
     await fetch(`${URL_BASE}/api/friends/${screenName}`, {
       method: 'PUT',
       body,
       headers: { 'Content-Type': 'application/json' },
     });
-    await this.getTweetData();
-    this.setState({ isFetchingData: !this.state.isFetchingData });
+    await getTweetData();
+    setIsFetchingData(false);
   };
 
-  ContentPanel = () => {
-    if (this.state.selectedFriend) {
+  const ContentPanel = () => {
+    if (selectedFriend) {
       return (
         <TweetPanel
-          friend={this.getFriend(this.state.selectedFriend)}
-          tweets={this.getTweetsByFriendId(this.state.selectedFriend)}
-          showAllTweets={this.state.showAllTweets}
-          onTweetRead={this.onTweetRead}
-          onTweetSave={this.onTweetSave}
-          onUserRead={this.onUserRead}
+          friend={getFriend(selectedFriend)}
+          tweets={getTweetsByFriendId(selectedFriend)}
+          showAllTweets={showAllTweets}
+          onTweetRead={onTweetRead}
+          onTweetSave={onTweetSave}
+          onUserRead={onUserRead}
         />
       );
-    } 
-    
+    }
+
     return <EmptyPanel />;
   }
 
-  buildSidebarFriendData() {
-    const sortedFriends = this.state.friends
-      .map((friend) => {
-        const f = friend.friend;
-        f.newTweetCount = friend.tweets.filter((tweet) => !tweet.isRead).length;
-        return f;
-      })
+  const buildSidebarFriendData = () => {
+    const sortedFriends = friends.map((friend) => {
+      const f = friend.friend;
+      f.newTweetCount = friend.tweets.filter((tweet) => !tweet.isRead).length;
+      return f;
+    })
       .sort((a, b) => {
         if (a.newTweetCount && !b.newTweetCount) {
           return -1;
-        } 
+        }
         if (b.newTweetCount && !a.newTweetCount) {
           return 1;
         }
@@ -155,35 +151,34 @@ class App extends Component {
       });
 
     // select the first friend in list
-    if (!this.state.selectedFriend && sortedFriends.length > 0) {
-      this.setState({ selectedFriend: sortedFriends[0]._id });
+    if (!selectedFriend && sortedFriends.length > 0) {
+      setSelectedFriend(sortedFriends[0]._id);
     }
 
     return sortedFriends;
   }
 
-  render() {
-    return (
-      <div className='App'>
-        <div className='app-error'>{this.state.error}</div>
-        <Header
-          className='app-header'
-          showAllTweets={this.state.showAllTweets}
-          onChangeShowAllTweets={this.onChangeShowAllTweets}
-          onRefreshTweets={this.onRefreshTweets}
-          isFetchingData={this.state.isFetchingData}
+  return (
+    <div className='App'>
+      <div className='app-error'>{error}</div>
+      <Header
+        className='app-header'
+        showAllTweets={showAllTweets}
+        onChangeShowAllTweets={onChangeShowAllTweets}
+        onRefreshTweets={onRefreshTweets}
+        isFetchingData={isFetchingData}
+      />
+      <main className='app-main'>
+        <Sidebar
+          friends={buildSidebarFriendData()}
+          selectedFriend={selectedFriend}
+          onFriendSelect={onFriendSelect}
         />
-        <main className='app-main'>
-          <Sidebar
-            friends={this.buildSidebarFriendData()}
-            selectedFriend={this.state.selectedFriend}
-            onFriendSelect={this.onFriendSelect}
-          />
-          <this.ContentPanel/>
-        </main>
-      </div>
-    );
-  }
+        <ContentPanel />
+      </main>
+    </div>
+  );
+
 }
 
 export default App;
